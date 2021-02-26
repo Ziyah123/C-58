9d498134b441aa33bb67e67a11d85355351f3a94
Ziyah123 <79238395+Ziyah123@users.noreply.github.com>
2021-02-26 09:40:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,2 @@
-function setdate(){
-document.getElementById("date").innerHTML=new date(). toDateString();
-}
+function profile() {
+document.getElementById("Ziyah Picture").src ="https://filerepository.itslearning.com/969e01c6-af9d-4bc5-bf69-3302c22ca1a2?637499436527741168"}
